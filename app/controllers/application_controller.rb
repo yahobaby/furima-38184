4d9dc26f5c32e2abc0aa993aab84f1a5eb4289ad
yahobaby <hellotechcamp@gmail.com>
2022-10-16 19:16:45 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  #basic認証1015////////
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      # username == 'hello' && password == 'techcamp'//環境変数設定済み1016
+    end
+  end
+  #basic認証ここまで1015////////
+
 end
