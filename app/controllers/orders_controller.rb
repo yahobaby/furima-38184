2a80d513ebaf59a1e273803fabe3578b1d5a9840
yahobaby <110825175+yahobaby@users.noreply.github.com>
2022-11-08 21:46:44 +0900

    third commit ①

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  # 「学習メモ」before_action：コントローラーの先に実行させる。もし、ログインしてないと、全てのアクションが使えない
+  before_action :order_item, only: [:index, :create]
+
   def index
   end
 
   def create
+  end
+
+  def order_item
+    # 他のコントローラーで生成されたitemを使う為、createアクションに定義。
+    @item = Item.find(params[:item_id])
   end
 end
