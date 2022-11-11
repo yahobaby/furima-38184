dd6795e490b7c6bba579ae335e1570f53618b185
yahobaby <hellotechcamp@gmail.com>
2022-11-11 10:55:42 +0900

    fifth commit - 単体モデル(order_address、クレジット決済者配送先情報)テストコードおよびその為のFactorybotファイル

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,111 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+
+  describe '配送先情報の保存' do
+    context '配送先情報の保存ができるとき' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'user_idが空でないと保存できる' do
+        @order_address.user_id = 1
+        expect(@order_address).to be_valid
+      end
+      it 'item_idが空でないと保存できる' do
+        @order_address.item_id = 1
+        expect(@order_address).to be_valid
+      end
+      it '郵便番号が「3桁+ハイフン+4桁」の組み合わせであれば保存できる' do
+        @order_address.post_code = '777-7777'
+        expect(@order_address).to be_valid
+      end
+      it '都道府県が「---」以外かつ空でなければ保存できる' do
+        @order_address.prefecture_id = 1
+        expect(@order_address).to be_valid
+      end
+      it '市区町村が空でなければ保存できる' do
+        @order_address.city = '沖縄市'
+        expect(@order_address).to be_valid
+      end
+      it '番地が空でなければ保存できる' do
+        @order_address.address = '西区7丁目7'
+        expect(@order_address).to be_valid
+      end
+      it '建物名が空でも保存できる' do
+        @order_address.building_name = ' '
+        expect(@order_address).to be_valid
+      end
+      it '電話番号が11番桁以内かつハイフンなしであれば保存できる' do
+        @order_address.phone_number = 77_777_777_777
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '配送先情報の保存ができないとき' do
+      it 'user_idが空なら保存できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空なら保存できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が空なら保存できない' do
+        @order_address.post_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code can't be blank", "Post code is invalid. Include hyphen(-)")
+      end
+      it '郵便番号にハイフンがなかったら保存できない' do
+        @order_address.post_code = 777_7777
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it '都道府県が「---」なら保存できない' do
+        @order_address.prefecture_id = 0
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '都道府県が空なら保存できない' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が空なら保存できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空なら保存できない' do
+        @order_address.address = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it '電話番号が空なら保存できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+      end
+      it '電話番号にハイフンがあるなら保存できない' do
+        @order_address.phone_number = '777-7777-7777'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が12桁以上あるなら保存できない' do
+        @order_address.phone_number = 12_345_678_910_123_456_789
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+      it 'トークンが空なら保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      
+    end
+  end
+end
