8c9b235f80208a5ec159629faaf701cfa9c819e5
yahobaby <hellotechcamp@gmail.com>
2023-01-08 11:37:51 +0900

    PAYJP v2 update

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,37 @@
 const pay = () => {
+  //環境変数をもとに公開鍵を復号
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY); 
+  //elementsインスタンスを生成
+  const elements = payjp.elements();
+  //入力欄ごとにelementインスタンスを生成
+  const numberElement = elements.create('cardNumber')
+  const cvcElement = elements.create('cardCvc')
+  const expiryElement = elements.create('cardExpiry')
+  //入力欄をDOM上に表示
+  numberElement.mount('#number')
+  cvcElement.mount('#cvc') 
+  expiryElement.mount('#exp-date')
+  //フォームの要素を取得
+  const form = document.getElementById("charge-form");
+  //PAY.JPと通信が成功した場合のみトークンをフォームに埋め込む
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    payjp.createToken(expiryElement).then((response) => {
+      if (response.error) {
+      } 
+      const token = response.id;
+      const renderDom = document.getElementById("charge-form"); 
+      const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+      renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
+
+/* PAYJP v1終了に伴い、v2へ切り替えの為
+const pay = () => {
   //「学習メモ」 PAY.JPテスト公開鍵
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   // 入力フォーム全体を、変数に定義
@@ -46,3 +79,4 @@ const pay = () => {
 
 //「学習メモ」 payページを読み込んだ時、実行
 window.addEventListener("load", pay);
+*/
